b3e9fe56b3146e84ccc48a47b27401e11e429c12
AndresFelipeJW <andresjimenez@colegioguanentaedu.co>
2023-02-15 09:17:49 -0500

    Se agrego el diagrama de flujo, programa y el README

--- invertir_numero_4digitos.py
+++ invertir_numero_4digitos.py
@@ -10,7 +10,13 @@ n = int(input("digite el NUMERO DE 4 DIGITOS PARA INVERTIR: "))
 
 #processing
 
-ni = ((n%10)*1000) + ((n//10)*1)
+d1 = ((n%10)*1000)
+pe = (n//10)
+d2 = ((pe%10)*100)
+pe = (pe//10)
+d3 = ((pe%10)*10)
+d4 = (pe//10)
+ni = (d1+d2+d3+d4)
 
 
 # output
